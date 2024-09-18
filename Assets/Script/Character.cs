3e3f33bffe4f61ad0ec7b35dcfcbf0fbcef5c694
krivanovapolina <kappolinariyak@gmail.com>
2024-09-18 21:05:15 +0300

    third commit

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -1,41 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class CharacterMove : MonoBehaviour
 {
-    [SerializeField]
-    float moveSpeed = 5f;
+    [Header("Movement Settings")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float cooldown = 3f;
+
+    [Header("References")]
+    [SerializeField] GameObject bulletPref;
+
     CharacterController controller;
+    Animator animator;
+
     Vector2 moveDirection;
     Vector3 mouseDirection;
-    float moveX;
-    float moveY;
-    Animator animator;
-    private float speedBullet = 5f;
-    private float lastAttack = -999f;
-    private float cooldown = 8f;
 
     float idleMoveX;
     float idleMoveY;
+    float moveX;
+    float moveY;
 
-    public GameObject bulletPref;
-    //public GameObject shootPoint;
+    bool canShoot = true;
+    int gunType = 0;
+    [Header("Melee Attack")]
+    [SerializeField] Transform meleeAttackPoint;
+    [SerializeField] float meleeAttackRange = 1f;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        HandleMovementInput();
+        HandleAnimation();
+        HandleChangeGun();
+
+        if (Input.GetMouseButtonDown(0) && canShoot)
+            StartCoroutine(Attack());
+
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    void Animate(float moveX, float moveY, int layer)
+    {
+        switch (layer)
+        {
+            case 0:
+                animator.SetLayerWeight(0, 1);
+                animator.SetLayerWeight(1, 0);
+                break;
+
+            case 1:
+                animator.SetLayerWeight(1, 1);
+                animator.SetLayerWeight(0, 0);
+                break;
+        }
+        animator.SetFloat("MoveX", moveX);
+        animator.SetFloat("MoveY", moveY);
+
+    }
+
+
+    Vector3 GetShootingDirection()
+    {
+
+        mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseDirection.z = 0f;
+        return (mouseDirection - transform.position).normalized;
+
+
+    }
+    void HandleMovementInput()
     {
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
-        moveDirection = new Vector2 (moveX, moveY).normalized;
-        if(moveX !=0 || moveY != 0)
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+    }
+    void HandleAnimation()
+    {
+        if (moveDirection != Vector2.zero)
         {
             Animate(moveX, moveY, 1);
             idleMoveX = moveX;
@@ -45,55 +100,66 @@ public class Character : MonoBehaviour
         {
             Animate(idleMoveX, idleMoveY, 0);
         }
-        if (Input.GetMouseButtonDown(0))
-            Attack();
-        else
-            animator.SetBool("IsAttack", false);
     }
 
-    private void FixedUpdate()
+    public void Shoot()
     {
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        Vector3 shootingDirection = GetShootingDirection();
+        GameObject bulletInstance = Instantiate(bulletPref, transform.position + GetShootingDirection(), Quaternion.identity);
+
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = shootingDirection * 5f;
+
+        float angle = Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg;
+        bulletInstance.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        Destroy(bulletInstance, 4f);
     }
 
-    private void Animate(float moveX, float moveY, int layer)
+    IEnumerator Attack()
     {
-        switch (layer)
+        canShoot = false;
+
+
+        animator.SetFloat("ShootX", GetShootingDirection().x);
+        animator.SetFloat("ShootY", GetShootingDirection().y);
+
+        switch (gunType)
         {
-            case 0: 
-                animator.SetLayerWeight(0, 1);
-                animator.SetLayerWeight(1, 0);
-            break;
+            case 0:
 
-            case 1:
-                animator.SetLayerWeight(1, 1);
-                animator.SetLayerWeight(0, 0);
+                animator.SetBool("IsAttack", true);
+
+                //Shoot();
                 break;
+            case 1:
+                animator.SetTrigger("Melee");
+                Collider2D enemy = Physics2D.OverlapCircle(meleeAttackPoint.position, meleeAttackRange);
+                Debug.Log(enemy.name);
+                break;
+
         }
-        animator.SetFloat("MoveX", moveX);
-        animator.SetFloat("MoveY", moveY);
+
+        yield return new WaitForSeconds(cooldown);
+
+        animator.SetBool("IsAttack", false);
+
+        canShoot = true;
     }
 
-    private void Attack()
+    void HandleChangeGun()
     {
-        if (Time.time > lastAttack + cooldown)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            gunType = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            animator.SetBool("IsAttack", true);
-            animator.SetFloat("MoveX", idleMoveX);
-            animator.SetFloat("MoveX", idleMoveY);
-
-            mouseDirection = Input.mousePosition;
-            Debug.Log(mouseDirection);
-            mouseDirection.z = 0f;
-            mouseDirection = Camera.main.ScreenToWorldPoint(mouseDirection);
-            Debug.Log(mouseDirection);
-            mouseDirection = mouseDirection - transform.position;
-
-            GameObject bulletInstance = Instantiate(bulletPref, transform.position, Quaternion.identity);
-            bulletInstance.GetComponent<Rigidbody2D>().velocity = mouseDirection * speedBullet;
-
-            Destroy(bulletInstance, 4);
-            lastAttack = Time.time;
+            gunType = 1;
         }
+        Debug.Log(gunType);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(meleeAttackPoint.position, meleeAttackRange);
     }
 }
